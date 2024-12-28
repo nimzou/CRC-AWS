ce837ecdd9ce66d6fc12ec94f2d1b76f7b13af37
Nihar <niharp362004.ni@gmail.com>
2024-12-28 21:55:55 +0530

    Role allowing lambda to access dynamodb

--- modules/function/main.tf
+++ modules/function/main.tf
@@ -34,3 +34,37 @@ resource "aws_dynamodb_table_item" "countdb" {
   ITEM
 }
 
+# IaM Role to Allow Lambda to access DynamoDB
+
+resource "aws_iam_role" "AllowDynamoDbAccess" {
+  name = "AllowDynamoDbAccess"
+  path = "/"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy" "dynamodb_access" {
+  name = "dynamodb_access"
+  role = aws_iam_role.AllowDynamoDbAccess.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Action = [
+        "dynamodb:GetItem",
+        "dynamodb:PutItem"
+      ]
+      Resource = aws_dynamodb_table.countdb.arn
+    }]
+  })
+}
