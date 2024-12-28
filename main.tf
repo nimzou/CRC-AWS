ba8ca2501c1f2a79252d19cd8571827bcd072c79
Nihar <niharp362004.ni@gmail.com>
2024-12-28 20:41:53 +0530

    Add website module

---
+++ main.tf
@@ -0,0 +1,11 @@
+module "website" {
+    source = "./modules/website"
+
+    aws_region           = var.aws_region
+    site_folder          = var.site_folder
+    site_index_file      = var.site_index_file
+    site_error_file      = var.site_error_file
+    site_domain          = var.site_domain
+    cloudflare_api_token = var.cloudflare_api_token
+    cloudflare_zone_id   = var.cloudflare_zone_id
+}
